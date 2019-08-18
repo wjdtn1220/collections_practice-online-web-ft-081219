66beb6bf9291c6e47382c4b89bb19ece475fc1c8
Jeong Park <wjdtn1220@gmail.com>
2019-08-18 02:17:26 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,11 +1,13 @@
 def sort_array_asc(array)
   array.sort do |a, b|
     a <=> b
+  end
 end
 
 def sort_array_desc(array)
   array.sort do |a, b|
     b <=> a
+  end
 end
 
 def sort_array_char_count(array)
